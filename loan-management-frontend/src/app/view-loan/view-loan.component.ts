f1381cd4d662d9eb86c5dc0d87629da70d30d53e
kamathak <ashwin.mite@gmail.com>
2020-06-29 10:38:36 +0530

    [#173506193,#173506206,#173506205]-Create Loan Search Screen, populate results, display error when no results.

--- loan-management-frontend/src/app/view-loan/view-loan.component.ts
+++ loan-management-frontend/src/app/view-loan/view-loan.component.ts
@@ -7,9 +7,73 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewLoanComponent implements OnInit {
 
+     borrowerFullName=""
+     dbBorrowerFullName
+     loanNumber=""
+     dbLoanNumber
+     loanAmount=""
+     dbLoanAmount
+     flag=false
+     found=false
+     notfound=false
+     deleted=false
+
+     loanDetails: any[] = [
+      {
+        "borrowerFullName": "Ashwin Kamath",
+        "loanNumber": "123",
+      "loanAmount": "123"
+      },
+      {
+        "borrowerFullName": "Sachin Kumar",
+        "loanNumber": "456",
+      "loanAmount": "456"
+      },
+      {
+        "borrowerFullName": "Anjana Shenoy",
+        "loanNumber": "789",
+      "loanAmount": "789"
+      }
+    ];
   constructor() { }
 
   ngOnInit(): void {
+    
+  }
+
+   searchLoan():void{
+
+    //logig to Search Loan
+    this.clearForm();
+    if(this.borrowerFullName.toLowerCase().includes("ashwin")||this.loanNumber.includes("1234")||this.loanAmount.includes("678"))
+    {
+      this.flag=true
+      this.found=true
+      this.dbBorrowerFullName="Ashwin Kamath"
+      this.dbLoanNumber="12345"
+      this.dbLoanAmount="67890"
+      
+      this.notfound=false
+    }
+    else{
+      this.notfound=true
+    }
+
+  }
+
+
+  deleteLoan(): void{
+
+//add logic to deleteLoan
+      this.flag=false
+      this.deleted=true
+      
+  }
+
+  clearForm(): void{
+    this.flag=false
+    this.deleted=false
+    this.notfound=false
   }
 
 }
