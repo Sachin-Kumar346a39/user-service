438b295ec72705793b077940a56b3f4200d16b58
Anjana <ctsjavafsd41@iiht.tech>
2020-07-01 15:22:46 +0530

    #173600390: Created DB Model & DDL & DML queries

---
+++ DB/smartweb_dml.sql
@@ -0,0 +1,2 @@
+INSERT INTO `smartweb`.`user` (`username`, `password`, `isAdmin`) VALUES ('admin@sw.com', 'pass', '1');
+INSERT INTO `smartweb`.`user` (`username`, `password`, `isAdmin`) VALUES ('user@sw.com', 'pass', '0');
