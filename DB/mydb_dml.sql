f4f31dcebaf2fa186c84f2401af953dcf81f2af9
Anjana <ctsjavafsd41@iiht.tech>
2020-07-04 17:23:17 +0530

    #173600390: Updated DB schema

---
+++ DB/mydb_dml.sql
@@ -0,0 +1,2 @@
+INSERT INTO `mydb`.`user` (`username`, `password`, `admin`) VALUES ('admin@sw.com', 'pass', '1');
+INSERT INTO `mydb`.`user` (`username`, `password`, `admin`) VALUES ('user@sw.com', 'pass', '0');
--- DB/smartweb_dml.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `smartweb`.`user` (`username`, `password`, `isAdmin`) VALUES ('admin@sw.com', 'pass', '1');
-INSERT INTO `smartweb`.`user` (`username`, `password`, `isAdmin`) VALUES ('user@sw.com', 'pass', '0');
