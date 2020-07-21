7b199339d69da699cea7ccdb42bcedf8642e44cd
kamathak <ashwin.mite@gmail.com>
2020-07-21 23:53:27 +0530

    #173918805- refactored the code

--- src/main/java/com/cts/user/Config.java
+++ src/main/java/com/cts/user/Config.java
@@ -1,11 +1,13 @@
 package com.cts.user;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.session.data.redis.config.ConfigureRedisAction;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -13,6 +15,11 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
 @EnableRedisHttpSession
 @Configuration
 public class Config extends WebSecurityConfigurerAdapter {
+	
+	@Bean
+	public static ConfigureRedisAction configureRedisAction() {
+	    return ConfigureRedisAction.NO_OP;
+	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
